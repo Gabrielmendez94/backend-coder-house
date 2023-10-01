88e779918cd664da52a491e8264282148bc6f49d
Gabriel Mendez <mendezgabriel.1994@gmail.com>
2023-09-30 23:52:48 -0300

    Implementando Swagger para documentación de mis API's

--- src/public/js/restore-pass.js
+++ src/public/js/restore-pass.js
@@ -1,14 +1,12 @@
 const newForm = document.getElementById('restorePass');
 
 newForm.addEventListener('submit', async e =>{
-    const newForm = document.getElementById('restorePass');
     e.preventDefault();
     const data = new FormData(newForm);
-    console.log(data);
     const obj = {};
     data.forEach((value, key)=>obj[key]= value);
-    const { email, newPassword } = obj;
-    const newObj = { email, newPassword };
+    const newPassword = obj;
+    const newObj = newPassword;
     await fetch(`/api/sessions/pass-change/${obj.token}`, {
         method: 'PUT',
         body: JSON.stringify(newObj),

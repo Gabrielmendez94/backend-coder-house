139203de31269d702b443852c2d9ca3d007bde56
Gabriel Mendez <mendezgabriel.1994@gmail.com>
2023-10-12 00:17:54 -0300

    Testing de products y carts finalizado

--- src/test/carts.test.js
+++ src/test/carts.test.js
@@ -19,8 +19,16 @@ describe("Endpoint de pruebas de los carritos", () => {
       });
 
       it("El endpoint GET '/api/carts/:cid' debe retornar un carrito específico correctamente", async ()=>{
-      });
+        
+        const cid = '64a499e7222a9a60e67c2f09';
+        const response = await requester.get(`/api/carts/${cid}`);
 
+        expect(response.status).to.equal(200);
+        expect(response.ok).to.equal(true);
+        expect(response.body).to.be('array');
+
+      });
+/* TO DO
       it("El endpoint PUT '/api/carts/:cid' debe actualizar un carrito correctamente", async ()=>{
       });
 
@@ -31,6 +39,6 @@ describe("Endpoint de pruebas de los carritos", () => {
       });
 
       it("El endpoint POST '/api/carts/:cid/checkout' debe redireccionarnos para finalizar una compra correctamente", async()=>{
-      });
+      });*/
   })
 });
--- src/test/products.test.js
+++ src/test/products.test.js
@@ -8,21 +8,42 @@ describe("Endpoint de pruebas de los productos", () => {
     describe("Prueba de productos", () => {
   
       it("El endpoint GET '/api/products' debe retornar todos los productos", async ()=> {
+        const response = await requester.get('/api/products');
+
+        expect(response.status).to.equal(200);
+        expect(response.ok).to.equal(true);
+        expect(response.body).to.be('array');
+
       });
 
-      it("El endpoint GET '/api/products/:pid' debe retornar un carrito específico correctamente", async ()=>{
+      it("El endpoint GET '/api/products/:pid' debe retornar un producto específico correctamente", async ()=>{
+        const pid = '64a0ca2bcf0a16babcee692f';
+        const response = await requester.get(`/api/products/${pid}`);
+
+        expect(response.status).to.equal(200);
+        expect(response.ok).to.equal(true);
+        expect(response.body).to.be('array');
       });
 
-      it("El endpoint PUT '/api/carts/:cid' debe actualizar un carrito correctamente", async ()=>{
+      it("El endpoint POST '/api/products' debe crear un producto correctamente", async ()=>{
+        const newDessert = {
+            title: 'Pancake',
+            description: 'Pancake Proteíco',
+            code: 2595671,
+            price: 499,
+            stock: 25,
+            category: 'Dessert',
+        };
+        const {statusCode, ok, _body} = await requester.post('/api/products').send(newDessert);
+        expect(statusCode).to.equal(201);
+        expect(ok).to.equal(true);
+        expect(_body).to.deep.include(newDessert);
+      });
+/* TO DO 
+      it("El endpoint PUT '/api/products/:pid' debe actualizar un producto especifico correctamente", async ()=>{
       });
 
-      it("El endpoint POST '/api/carts/:cid/product/:pid' debe agregar un producto especifico en un carrito especifico correctamente", async ()=>{
-      });
-
-      it("El endpoint DELETE '/api/carts/:cid/product/:pid' debe eliminar un producto especifico en un carrito especifico correctamente", async ()=>{
-      });
-
-      it("El endpoint POST '/api/carts/:cid/checkout' debe redireccionarnos para finalizar una compra correctamente", async()=>{
-      });
+      it("El endpoint DELETE '/api/products/:pid' debe eliminar un producto especifico correctamente", async ()=>{
+      });*/
   })
 });
